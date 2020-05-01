4731d90b9056305529e6a0d7767d24ae508cde60
intruderF <lmwolejko@gmail.com>
2020-05-01 15:27:57 +0200

    Text file connection for Prize Form

--- TrackerLibrary/DataAccess/TextConnector.cs
+++ TrackerLibrary/DataAccess/TextConnector.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Text;
 using TrackerLibrary.Models;
+using TrackerLibrary.DataAccess.TextHelpers;
+using System.Linq;
 
 namespace TrackerLibrary.DataAccess
 {
     public class TextConnector : IDataConnection
     {
+        private const string PrizesFile = "PrizeModels.csv";
+
         // TODO -> Make the CreatePrize method actually save to the text file
         /// <summary>
         /// Saves a new prize to the text file.
@@ -15,7 +19,25 @@ namespace TrackerLibrary.DataAccess
         /// <returns>The prize information, including the unique identifier.</returns>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            model.Id = 1;
+            // Load the text file and convert the text to List<PrizeModel>
+            List<PrizeModel> prizes = PrizesFile.FullFilePath().LoadFile().ConvertToPrizeModels();
+
+            // Find the max ID
+            int currentId = 1;
+
+            if (prizes.Count > 0)
+            {
+                currentId = prizes.OrderByDescending(x => x.Id).First().Id + 1;
+
+            }
+            model.Id = currentId;
+
+            // Add the new record with the new ID (max + 1)
+            prizes.Add(model);
+
+            // Convert the prizes to List<string>
+            // Save the List<string> to the text file
+            prizes.SaveToPrizeFile(PrizesFile);
 
             return model;
         }
